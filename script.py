19a7202d6070a37ae39b87f7a6faa9c135f0f0cd
Rafael Vázquez <tota77@DeLorean.local>
2019-05-26 12:53:57 -0500

    se anñade funcion de scaneo

---
+++ script.py
@@ -0,0 +1,23 @@
+import nmap 
+import subprocess
+import time
+
+
+network="192.168.88.0/24"
+def scan():
+	p = subprocess.Popen("sudo nmap -sn 192.168.88.0/24 | grep 'MAC Address:' | awk '{print $3}'" ,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	(output, err) = p.communicate()
+	p_status = p.wait()
+	print("Command output : ")
+	print(output.decode('utf-8').strip())
+
+def main():
+	scan()
+
+main()
+# nm = nmap.PortScanner()
+# nm.scan(hosts='192.168.88.0/24', arguments='-sn')
+# nm.command_line()
+# hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+# for host, status in hosts_list:
+# 	print('{0}:{1}'.host)

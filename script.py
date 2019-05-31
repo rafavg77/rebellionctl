c07cbd5f10221f538cf5d1858e100a82a252bb84
rafavg77 <rafavg77@gmail.com>
2019-05-30 23:37:22 -0500

    Se agrega funcionalidad para obtener direcciones MAC y guardarlas en Google Spreadsheet

--- script.py
+++ script.py
@@ -1,19 +1,44 @@
+#!/usr/bin/env python
+
 import nmap 
-import subprocess
 import time
 import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 network="192.168.88.0/24"
 
 def scan():
-	p = subprocess.Popen("sudo nmap -sn 192.168.88.0/24 | grep 'MAC Address:' | awk '{print $3}'" ,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	(output, err) = p.communicate()
-	p_status = p.wait()
-	#print("Command output : ")
-	print(output.decode('utf-8').strip())
-	x = datetime.datetime.now()
-	print(x)
+	nm = nmap.PortScanner() 
+	cidr2='192.168.88.0/24'
+	date=str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+	a=nm.scan(hosts=cidr2, arguments='-sP') 
+
+	macs = []
+	for k,v in a['scan'].iteritems(): 
+		if str(v['status']['state']) == 'up':
+			try:    
+				print str(v['addresses']['mac'])
+				macs.append(str(v['addresses']['mac']))
+			except: print str(v['addresses']['ipv4'])
+	
+	logSpreadsheet(macs,date)
+
+def logSpreadsheet(macs,date):
+	scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+	creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/tota77/Developer/client_secret.json', scope)
+	client = gspread.authorize(creds)
+	sheet = client.open("Monitoreo-Casa").sheet1
+
+	for mac in macs:
+		print date, mac
+		row = [date,str(mac),"DeLorear"]
+		print row
+		index = 1
+		sheet.append_row(row)
+
 
 def main():
 	scan()
